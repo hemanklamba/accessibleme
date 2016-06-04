8191515e74a1b9799924cbf4506ec05d817b5639
Mahdi Moqri <Mahdi@mohammadmahdis-MacBook-Air.local>
2016-06-04 12:49:26 -0400

    add db

--- backendstuff/main_app/views.py
+++ backendstuff/main_app/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
+from .models import Report
+
 
 
 def index(request):
     return HttpResponse("we are alive!")
+
+def report(request):
+    all_reports = Report.objects.all()
+    output = ', '.join(['_'.join([rep.lat,rep.lon]) for rep in all_reports])
+    return HttpResponse(output)
